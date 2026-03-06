3d629d96357307229bb3d619a5a4e3b956a2a7e6
namharledba <abdulrahmanmo186@gmail.com>
2026-03-06 23:19:37 +0200

    fixed some minor issues

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 from Functions import false_position,bi_section,simple_fixed_point
-result = false_position(0,1,"4*x**3-6*x**2+7*x-2.3",0.5)
+result = simple_fixed_point(5,"0.9*x**2+1.7*x+2.5",0.7)
 print(result)
