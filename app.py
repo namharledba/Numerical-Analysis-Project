35c65bd563d403d91ce8ac49765266bd7245ec98
namharledba <abdulrahmanmo186@gmail.com>
2026-03-07 21:29:26 +0200

    fixed some minor issues

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
-from Functions import false_position,bi_section,simple_fixed_point
-result = simple_fixed_point(5,"0.9*x**2+1.7*x+2.5",0.7)
+from Functions import newton_method
+
+result = newton_method(5,"-0.9*x**2+1.7*x+2.5",0.7)
 print(result)
