464863efdb745423827aa8249c96f7646d4abf28
Abd El-Rahman <abdulrahmanmo186@gmail.com>
2026-03-18 05:27:46 +0200

    Integrate SQLite for polynomial storage and update functions
    Added SQLite database connection and table creation for storing polynomial data. Updated display and validation functions to improve error handling and output formatting.

--- Functions.py
+++ Functions.py
@@ -1,16 +1,20 @@
 import sympy as sp
+import sqlite3 as sql
+
+
+db = sql.connect("Numerical_Analysis.db")
+cr = db.cursor()
+
+cr.execute("CREATE TABLE if not exists polynomials(xl DOUBLE, xu DOUBLE, equ TEXT, expected_error DOUBLE)")
+cr.execute("insert into polynomials values(?,?,?,?) ")
+
 
 def display (i,xl,fxl,xu,fxu,xr,fxr,error) :
-    if i == 0 :
-        print(f"iteration :{i} | xl = {xl:.3f} | f(xl) = {fxl:.3f} | xu = {xu:.3f}|f(xu) = {fxu:.3f} | xr = {xr:.3f} |"
-          f" f(xr) = {fxr:.3f} |error = {"____"}")
-    else :
-        print(f"iteration :{i} | xl = {xl:.3f} | f(xl) = {fxl:.3f} | xu = {xu:.3f}|f(xu) = {fxu:.3f} | xr = {xr:.3f} |"
-              f" f(xr) = {fxr:.3f} |error = {error:.3f} ")
+    print(f"iteration :{i} | xl = {xl:.3f} | f(xl) = {fxl:.3f} | xu = {xu:.3f}|f(xu) = {fxu:.3f} | xr = {xr:.3f} |"
+          f" f(xr) = {fxr:.3f} |error = {error:.3f} %")
 
-def not_valid(fxu, fxl):
-        return fxu * fxl > 0
-
+def valid(fxu, fxl):
+        return fxu * fxl < 0
 
 def simple_fixed_point(frist_initial, equ, expected_error):
     x = sp.symbols('x')
@@ -27,12 +31,12 @@ def simple_fixed_point(frist_initial, equ, expected_error):
     error = 100
     i = 0
 
-    while error >= expected_error :
+    while error != expected_error :
         gx = gx_expr.subs(x, x_value)
         if i == 0:
             print(f"i = {i} | X = {x_value:.3f} | gx = {gx:.3f} | error = ____")
         else :
-            print(f"i = {i} | X = {x_value:.3f} | gx = {gx:.3f} | error = {error:.3f}")
+            print(f"i = {i} | X = {x_value:.3f} | gx = {gx:.3f} | error = {error:.3f} %")
         if error <= expected_error: break
         error = abs((gx - x_value) / gx) * 100
         x_value = gx
@@ -43,13 +47,12 @@ def bi_section(xl, xu, equ, expected_error):
     x = sp.symbols('x')
     fx = sp.sympify(equ)
     xr= 0.00
-    error = 100
     i = 0
     fxu = fx.subs(x, xu)
     fxl = fx.subs(x, xl)
 
-    while error >= expected_error :
-        if not_valid(fxu, fxl):
+    while valid(fxu,fxl) :
+        if not valid(fxu,fxl):
             print("The function has no solution...")
             break
         xr_old = xr
@@ -74,18 +77,16 @@ def false_position(xl, xu, equ, expected_error):
     x = sp.symbols('x')
     fx = sp.sympify(equ)
     xr = 0.00
-    error = 100
     i = 0
-    while error >= expected_error:
-        xr_old = xr
+    fxl = fx.subs(x, xl)
+    fxu = fx.subs(x, xu)
 
-        fxl = fx.subs(x, xl)
-        fxu = fx.subs(x, xu)
-
-        if fxu * fxl > 0:
+    while valid(fxu,fxl) :
+        if not valid(fxu,fxl):
             print("The function has no solution...")
             break
 
+        xr_old = xr
         xr = xu - ((fxu * (xl - xu)) / (fxl - fxu))
         fxr = fx.subs(x, xr)
         error = abs((xr - xr_old) / xr) * 100
@@ -93,8 +94,10 @@ def false_position(xl, xu, equ, expected_error):
         display(i,xl,fxl,xu,fxu,xr,fxr,error)
         if fxr * fxl < 0:
             xu = xr
+            fxu = fx.subs(x, xu)
         elif fxr * fxl > 0:
             xl = xr
+            fxl = fx.subs(x, xl)
         else:
             break
         if error <= expected_error: break
@@ -109,7 +112,7 @@ def newton_method(initial, equ, expected_error):
     error = 100
     i = 0
 
-    while error >= expected_error:
+    while error != expected_error:
         fx_val = float(fx.subs(x, x_value))
         dfx_val = float(dfx.subs(x, x_value))
 
@@ -129,4 +132,29 @@ def newton_method(initial, equ, expected_error):
         x_value = xr
         i += 1
 
-    print(f"\nRoot = {xr:.6f}")
+def se_cant(xi, xi_1, equ, expected_error):
+
+    x = sp.symbols('x')
+    fx = sp.sympify(equ)
+
+    error = 100
+    i = 0
+    while error != expected_error:
+
+        xi_old = xi
+
+        fxi = float(fx.subs(x, xi))
+        fxi_1 = float(fx.subs(x, xi_1))
+
+        xi_new = xi - (fxi * (xi_1 - xi)) / (fxi_1 - fxi)
+
+        error = abs((xi_new - xi_old) / xi_new) * 100
+
+        print(f"iteration:{i}|{xi_1:.3f}|{fxi_1:.3f}|{xi:.3f}|{fxi:.3f}|{error:.3f}")
+
+        xi_1 = xi
+        xi = xi_new
+
+        i += 1
+    return xi
+
