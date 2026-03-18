5db0ab24d4d1a592320f0b06510cd39b7a10a488
Abd El-Rahman <abdulrahmanmo186@gmail.com>
2026-03-18 05:28:23 +0200

    Integrate Streamlit and update function imports
    Updated app.py to import all functions and integrate Streamlit for UI.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
-from Functions import false_position,bi_section,simple_fixed_point
-result = false_position(0,1,"4*x**3-6*x**2+7*x-2.3",0.5)
-print(result)
+from Functions import *
+import streamlit as st
+
+
+st.title("Numerical Equation solver")
