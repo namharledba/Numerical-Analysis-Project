a70d98f66ce2a5d33a9061216e22bceb8a2c41da
Abd El-Rahman <abdulrahmanmo186@gmail.com>
2026-03-30 19:53:39 +0200

    Update title and buttons in app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from Functions import *
 import streamlit as st
 
-
-st.title("Numerical Equation solver")
+st.title("NUMERICAL ANALYSIS")
+st.write("----------------")
+st.write("Choose a method to use : ")
+st.button("Bisection")
+st.button("False Position")
+st.button("newton's method")
+st.button("Se cant")
+st.button("Gauss Elimination")
+st.button("L & U method")
