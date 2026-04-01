40c7333c92f29d405ac752787b1e1efe95521533
Abd El-Rahman <abdulrahmanmo186@gmail.com>
2026-04-01 14:48:34 +0200

    Refactor form submission buttons for methods

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 def polynomials():
     st.session_state.option = 1
 
+
 def linearalgebra():
     st.session_state.option = 2
 
@@ -51,14 +52,88 @@ elif st.session_state.option == 1:
                 xu = st.number_input("Enter xu")
                 equ = st.text_input("Enter equation f(x)")
                 expected_error = st.number_input("Enter expected error")
-                if st.form_submit_button("Submit"):
+                if st.form_submit_button("Submit",key = "Bisection"):
+                    st.session_state.option = 0
                     result = bi_section(xl, xu, equ, expected_error)
-                    st.table(result)
+                    if not valid(xu, xl,equ):
+                        st.error("invalid input")
+                    elif not valid_error(expected_error):
+                        st.error("Expected error is out of range (0:100)")
+                    else:
+                        st.success("here's the result")
+                        st.session_state["df"] = result
+                        st.session_state["row_index"] = 0
+            if "df" in st.session_state:
+                df = st.session_state["df"]
+                row_index = st.session_state.get("row_index", 0)
 
+                st.dataframe(df.iloc[: row_index + 1])
+
+                if row_index < len(df) - 1:
+                    col1, col2 = st.columns([1, 1])
+
+                    with col1:
+                        if st.button("Next Row",key = "Bisection"):
+                            st.session_state["row_index"] += 1
+                            st.rerun()
+
+                    with col2:
+                        if st.button("Show All",key = "Bisection"):
+                            st.session_state["row_index"] = len(df) - 1
+                            st.rerun()
+                else:
+                    st.info("All rows displayed.")
 
         with tab2 :
             st.subheader("False position method:")
-            st.write("")
+            st.write("<small>Although bisection is a perfectly valid technique for"
+                     "determining roots, its “brute-force” approach is relatively"
+                     "inefficient. False position is an alternative based on a graphical"
+                     "insight. A shortcoming of the bisection method is that, in dividing"
+                     "the interval from xl to xu into equal halves, no account is taken of"
+                     "the magnitudes of f (xl) and f (xu). For example, if f (xl) is much"
+                     "closer to zero than f (xu), it is likely that the root is closer to xl than to xu The fact that the replacement of the curve by a straight line"
+                     "gives a “false position” of the root is the origin of the name, method"
+                     "of false position, or in Latin, <u>regula falsi</u>. It is also called the <u>linear interpolation method</u>.</small>", unsafe_allow_html=True)
+            st.write("Make sure that f(x) is real and continuous in the interval from xl to xu and f(xl) and f(xu) have opposite signs")
+            with st.form("False position"):
+                xl = st.number_input("Enter xl")
+                xu = st.number_input("Enter xu")
+                equ = st.text_input("Enter equation f(x)")
+                expected_error = st.number_input("Enter expected error")
+                if st.form_submit_button("Submit",key="False position"):
+                    result = false_position(xl, xu, equ, expected_error)
+                    if not valid(xu, xl, equ):
+                        st.error("invalid input")
+                    elif not valid_error(expected_error):
+                        st.error("Expected error is out of range (0:100)")
+                    else:
+                        st.success("here's the result")
+                        st.session_state["df"] = result
+                        st.session_state["row_index"] = 0
+            if "df" in st.session_state:
+                df = st.session_state["df"]
+                row_index = st.session_state.get("row_index", 0)
+
+                st.dataframe(df.iloc[: row_index + 1])
+
+                if row_index < len(df) - 1:
+                    col1, col2 = st.columns([1, 1])
+
+                    with col1:
+                        if st.button("Next Row",key="False position"):
+                            st.session_state["row_index"] += 1
+                            st.rerun()
+
+                    with col2:
+                        if st.button("Show All",key="False position"):
+                            st.session_state["row_index"] = len(df) - 1
+                            st.rerun()
+                else:
+                    st.info("All rows displayed.")
+
+
+
         with tab3 :
             st.subheader("Simple Fixed point method:")
             st.write("")
