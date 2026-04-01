e3f803f4c070274ff14028a7722c761d71071aa0
Abd El-Rahman <abdulrahmanmo186@gmail.com>
2026-04-01 15:05:28 +0200

    Refactor Functions.py for code consistency and clarity

--- Functions.py
+++ Functions.py
@@ -5,20 +5,24 @@ from numpy.matlib import zeros
 import pandas as pd
 from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application
 
+
 def parse_function(equ):
     equ = equ.replace("^", "**")
     transformations = standard_transformations + (implicit_multiplication_application,)
     return sp.parse_expr(equ, transformations=transformations)
 
+
 db = sql.connect("Numerical_Analysis.db")
 cr = db.cursor()
 
-def valid(xu, xl,equ):
+
+def valid(xu, xl, equ):
     x = sp.symbols('x')
     fx = parse_function(equ)
     fxu = fx.subs(x, xu)
     fxl = fx.subs(x, xl)
-    return round(fxu,8) * round(fxl,8) < 0
+    return round(fxu, 8) * round(fxl, 8) < 0
+
 
 def valid_error(expected_error):
     return 0 < expected_error < 100
@@ -31,19 +35,19 @@ def simple_fixed_point(frist_initial, equ, expected_error):
     coeff_highest = fx.coeff(x ** highest_power)
 
     reset = fx - (coeff_highest * x ** highest_power)
-    if coeff_highest < 0 :
+    if coeff_highest < 0:
         gx_expr = (-reset / coeff_highest) ** (1 / highest_power)
-    else :
+    else:
         gx_expr = (reset / coeff_highest) ** (1 / highest_power)
     x_value = float(frist_initial)
     error = 100
     i = 0
 
-    while error != expected_error :
+    while error != expected_error:
         gx = gx_expr.subs(x, x_value)
         if i == 0:
             print(f"i = {i} | X = {x_value:.3f} | gx = {gx:.3f} | error = ____")
-        else :
+        else:
             print(f"i = {i} | X = {x_value:.3f} | gx = {gx:.3f} | error = {error:.3f} %")
         if error <= expected_error: break
         error = abs((gx - x_value) / gx) * 100
@@ -51,43 +55,44 @@ def simple_fixed_point(frist_initial, equ, expected_error):
         i += 1
 
 
-
 def bi_section(xl, xu, equ, expected_error):
     x = sp.symbols('x')
     fx = parse_function(equ)
-    xr= 0.00
+    xr = 0.00
     i = 0
     fxu = fx.subs(x, xu)
     fxl = fx.subs(x, xl)
     df = pd.DataFrame(columns=['Xl', 'Fxl', 'Xu', 'Fxu', 'Xr', 'Fxr', 'Error'])
 
-    while valid(xu,xl,equ) :
+    while valid(xu, xl, equ):
         xr_old = xr
         xr = (xl + xu) / 2.0
         fxr = fx.subs(x, xr)
 
         error = abs((xr - xr_old) / xr) * 100
         new_row_data = pd.DataFrame([{
-            'Xl':round(xl,3),'Fxl':round(fxl,5) ,'Xu':round(xu,3),'Fxu':round(fxu,5),
-            'Xr':round(xr,3),'Fxr':round(fxr,5),'Error':round(error,5)
-                 }])
-        df = pd.concat([df,new_row_data],ignore_index=True)
+            'Xl': round(xl, 3), 'Fxl': round(fxl, 5), 'Xu': round(xu, 3), 'Fxu': round(fxu, 5),
+            'Xr': round(xr, 3), 'Fxr': round(fxr, 5), 'Error': round(error, 5)
+        }])
+        df = pd.concat([df, new_row_data], ignore_index=True)
 
         if fxr * fxl < 0:
-                xu = xr
-                fxu = fx.subs(x, xu)
+            xu = xr
+            fxu = fx.subs(x, xu)
         elif fxr * fxl > 0:
-                xl = xr
-                fxl = fx.subs(x, xl)
+            xl = xr
+            fxl = fx.subs(x, xl)
         else:
-                break
-        if error <= expected_error: break
-        else: i+=1
+            break
+        if error <= expected_error:
+            break
+        else:
+            i += 1
 
-    if not valid(xu,xl,equ) :
-            return None
+    if not valid(xu, xl, equ):
+        return None
     else:
-            return df
+        return df
 
 
 def false_position(xl, xu, equ, expected_error):
@@ -99,16 +104,16 @@ def false_position(xl, xu, equ, expected_error):
     fxu = fx.subs(x, xu)
     df = pd.DataFrame(columns=['Xl', 'Fxl', 'Xu', 'Fxu', 'Xr', 'Fxr', 'Error'])
 
-    while valid(fxu,fxl,equ) :
+    while valid(fxu, fxl, equ):
         xr_old = xr
         xr = xu - ((fxu * (xl - xu)) / (fxl - fxu))
         fxr = fx.subs(x, xr)
         error = abs((xr - xr_old) / xr) * 100
 
         new_row_data = pd.DataFrame([{
-            'Xl': round(xl, 3), 'Fxl': round(fxl, 5), 'Xu': round(xu, 3), 'Fxu': round(fxu, 5),
-            'Xr': round(xr, 3), 'Fxr': round(fxr, 5), 'Error': round(error, 5)
-        }])
+                    'Xl': round(xl, 3), 'Fxl': round(fxl, 5), 'Xu': round(xu, 3), 'Fxu': round(fxu, 5),
+                    'Xr': round(xr, 3), 'Fxr': round(fxr, 5), 'Error': round(error, 5)
+                }])
         df = pd.concat([df, new_row_data], ignore_index=True)
 
         if fxr * fxl < 0:
@@ -125,40 +130,39 @@ def false_position(xl, xu, equ, expected_error):
 
 
 def newton_method(initial, equ, expected_error):
-
-    x       =  sp.symbols('x')
-    fx      =  sp.sympify(equ)
-    dfx     =  sp.diff(fx, x)
-    xi      =  round(initial,3)
-    error   =  100
-    i       =  0
+    x = sp.symbols('x')
+    fx = sp.sympify(equ)
+    dfx = sp.diff(fx, x)
+    xi = round(initial, 3)
+    error = 100
+    i = 0
 
     while True:
 
-        fx_val   = fx.subs( x , xi )
-        dfx_val  = dfx.subs( x , xi )
-        xi_plus1 = round(xi - ( fx_val / dfx_val ),3)
+        fx_val = fx.subs(x, xi)
+        dfx_val = dfx.subs(x, xi)
+        xi_plus1 = round(xi - (fx_val / dfx_val), 3)
 
         if i == 0:
-            print(f"iteration :{i} | x = {xi} | f(x) = {round(fx_val,4)} | f'(x) = {round(dfx_val,3)} | error = ____")
+            print(f"iteration :{i} | x = {xi} | f(x) = {round(fx_val, 4)} | f'(x) = {round(dfx_val, 3)} | error = ____")
         else:
-            print(f"iteration :{i} | x = {xi} | f(x) = {round(fx_val,4)} | f'(x) = {round(dfx_val,3)} | error = {error}%")
+            print(
+                f"iteration :{i} | x = {xi} | f(x) = {round(fx_val, 4)} | f'(x) = {round(dfx_val, 3)} | error = {error}%")
 
         if error <= expected_error: break
 
-        error = round(abs((xi_plus1 - xi) / xi_plus1) * 100,3)
-        xi    = xi_plus1
-        i    += 1
+        error = round(abs((xi_plus1 - xi) / xi_plus1) * 100, 3)
+        xi = xi_plus1
+        i += 1
+
 
 def se_cant(xi, xi_1, equ, expected_error):
-
     x = sp.symbols('x')
     fx = sp.sympify(equ)
 
     error = 100
     i = 0
     while error != expected_error:
-
         xi_old = xi
 
         fxi = float(fx.subs(x, xi))
@@ -175,74 +179,66 @@ def se_cant(xi, xi_1, equ, expected_error):
         i += 1
 
 
-
 def gauss_elimination(ab):
-    temp_matrix = np.array(ab,float)
-    shape       = temp_matrix.shape
-    no_columns  = shape[1]
-    no_rows     = shape[0]
+    temp_matrix = np.array(ab, float)
+    shape = temp_matrix.shape
+    no_columns = shape[1]
+    no_rows = shape[0]
     a = temp_matrix[:, :-1]
     b = temp_matrix[:, -1]
     x = zeros(no_rows)
 
     for i in range(no_rows):
-            b[i] = ab[i][-1]
+        b[i] = ab[i][-1]
 
     for i in range(no_rows):
-        for j in range(no_columns-1):
+        for j in range(no_columns - 1):
             a[i][j] = ab[i][j]
 
-
-    for k in range(no_rows-1):
-        for i in range(k+1, no_rows):
+    for k in range(no_rows - 1):
+        for i in range(k + 1, no_rows):
             if a[i][k] == 0: continue
             factor = a[i][k] / a[k][k]
-            for j in range(k,no_columns-1):
+            for j in range(k, no_columns - 1):
                 a[i][j] = a[i][j] - (a[k][j] * factor)
             b[i] = b[i] - (b[k] * factor)
 
-    x[no_rows-1] = b[no_rows-1] / a[no_rows-1][no_rows-1]
+    x[no_rows - 1] = b[no_rows - 1] / a[no_rows - 1][no_rows - 1]
 
     for i in range(no_rows - 1, -1, -1):
         sum_x = 0
         for j in range(i + 1, no_rows):
-                sum_x += a[i][j] * x[j]
-                x[i] = (b[i] - sum_x) / a[i][i]
+            sum_x += a[i][j] * x[j]
+            x[i] = (b[i] - sum_x) / a[i][i]
 
 
 def lu_decomposition(ab):
-   
     temp_matrix = np.array(ab, float)
     n = temp_matrix.shape[0]
     A = temp_matrix[:, :-1]
     b = temp_matrix[:, -1]
-    
-   
-    L = np.eye(n) 
+
+    L = np.eye(n)
     U = np.zeros((n, n))
-    
- 
+
     for i in range(n):
-       
+
         for k in range(i, n):
             sum_lu = sum(L[i][j] * U[j][k] for j in range(i))
             U[i][k] = A[i][k] - sum_lu
-            
-        
+
         for k in range(i + 1, n):
             sum_lu = sum(L[k][j] * U[j][i] for j in range(i))
             L[k][i] = (A[k][i] - sum_lu) / U[i][i]
 
-   
     y = np.zeros(n)
     for i in range(n):
         sum_ly = sum(L[i][j] * y[j] for j in range(i))
         y[i] = b[i] - sum_ly
-        
-   
+
     x = np.zeros(n)
     for i in range(n - 1, -1, -1):
         sum_ux = sum(U[i][j] * x[j] for j in range(i + 1, n))
         x[i] = (y[i] - sum_ux) / U[i][i]
-        
+
     return L, U, x
