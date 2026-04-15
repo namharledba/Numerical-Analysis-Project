9a6f00d0796c13bf5cfb2aeef50905fc7644a2d6
Abd El-Rahman <abdulrahmanmo186@gmail.com>
2026-04-16 01:29:49 +0200

    Refactor numerical methods with max iterations and error handling

--- Functions.py
+++ Functions.py
@@ -5,6 +5,10 @@ from numpy.matlib import zeros
 import pandas as pd
 from sympy.parsing.sympy_parser import standard_transformations, implicit_multiplication_application
 
+def set_xr(xl, xu):
+        result = (xl + xu) / 2
+        return result
+
 
 def parse_function(equ):
     equ = equ.replace("^", "**")
@@ -28,109 +32,132 @@ def valid_error(expected_error):
     return 0 < expected_error < 100
 
 
-def simple_fixed_point(frist_initial, equ, expected_error):
+def set_max_iterations_(max_iterations):
+    if max_iterations == 0:
+        return 50
+    if max_iterations < 0:
+        return None
+    else:
+        return max_iterations
+
+
+
+def simple_fixed_point(frist_initial, equ, expected_error, max_iterations):
+    
     x = sp.symbols('x')
     fx = parse_function(equ)
+    iterations = set_max_iterations_(max_iterations)
+
     highest_power = sp.degree(fx)
     highest_coefficient = fx.coeff(x ** highest_power)
-
     reset = fx - (highest_coefficient * x ** highest_power)
     if highest_coefficient < 0:
         gx_expr = (-reset / highest_coefficient) ** (1 / highest_power)
     else:
         gx_expr = (reset / highest_coefficient) ** (1 / highest_power)
-    x_value = float(frist_initial)
+    
+    xi = float(frist_initial)
     error = 100
-    i = 0
+
     df = pd.DataFrame(columns=['X', 'gx', 'Error'])
     while error != expected_error:
-        gx = gx_expr.subs(x, x_value)
+        
+        gx = gx_expr.subs(x, xi)
 
-        if i == 0:
+        if iterations == set_max_iterations_(max_iterations) :
             new_row_data = pd.DataFrame([{
-                'X': round(x, 3), 'gx': round(gx, 5), 'Error': "---"
+                'X': xi, 'gx': gx, 'Error': "---"
             }])
         else:
             new_row_data = pd.DataFrame([{
-                'X': round(x, 3), 'gx': round(gx, 5), 'Error': round(error, 5)
+                'X': xi, 'gx': gx, 'Error': error
             }])
 
         df = pd.concat([df, new_row_data], ignore_index=True)
-        if error <= expected_error: break
-        error = abs((gx - x_value) / gx) * 100
-        x_value = gx
-        i += 1
-    return df
+        
+        if error <= expected_error or iterations == 0:
+            break
+        
+        error = abs((gx - xi) / gx) * 100
+        xi = gx
+        iterations -= 1
+    if df.empty: return None
+    else: return df
 
-def bi_section(xl, xu, equ, expected_error):
+
+def bi_section(xl, xu, equ, expected_error, max_iterations):
+    
     x = sp.symbols('x')
     fx = parse_function(equ)
-    xr = 0.00
-    i = 0
-    error = 100
+    
+    iterations = set_max_iterations_(max_iterations)
+    xr_old = 0.00
     fxu = fx.subs(x, xu)
     fxl = fx.subs(x, xl)
-    df = pd.DataFrame(columns=['Xl', 'Fxl', 'Xu', 'Fxu', 'Xr', 'Fxr', 'Error'])
+    
+    df = pd.DataFrame(columns=['xl', 'f(xl)', 'xu', 'f(xu)', 'xr', 'f(xr)', 'Error'])
 
     while valid(xu, xl, equ):
-        if not valid(xu, xl, equ):
-            return 1
-        xr_old = xr
-        xr = (xl + xu) / 2.0
+        xr = set_xr(xl, xu)
         fxr = fx.subs(x, xr)
+        error = abs((xr - xr_old) / xr) * 100
 
-        if i == 0 :
-
+        if iterations == set_max_iterations_(max_iterations) :
             new_row_data = pd.DataFrame([{
-                'Xl': xl, 'Fxl': fxl, 'Xu': xu, 'Fxu': fxu,
-                'Xr': xr, 'Fxr': fxr, 'Error': "---"
+                'xl': xl, 'f(xl)': fxl, 'xu': xu, 'f(xu)': fxu,
+                'xr': xr, 'f(xr)': fxr, 'Error': "---"
             }])
         else:
             new_row_data = pd.DataFrame([{
-            'Xl': xl, 'Fxl':fxl, 'Xu': xu, 'Fxu': fxu,
-            'Xr': xr, 'Fxr': fxr, 'Error': error
+            'xl': xl, 'f(xl)': fxl, 'xu': xu, 'f(xu)': fxu,
+            'xr': xr, 'f(xr)': fxr, 'Error': error
             }])
-            error = abs((xr - xr_old) / xr) * 100
+
         df = pd.concat([df, new_row_data], ignore_index=True)
+        
         if fxr * fxl < 0:
             xu = xr
             fxu = fx.subs(x, xu)
         elif fxr * fxl > 0:
             xl = xr
-            fxl = fx.subs(x, xl)
-        else:
+            fxl = fx.subs(x, xl)       
+        if error <= expected_error or iterations == 0:
             break
-        if error <= expected_error :
-            break
-        i += 1
-    return df
+        iterations -= 1 
+        xr_old = xr
+    
+    if df.empty: return None
+    else: return df
 
 
-def false_position(xl, xu, equ, expected_error):
+def false_position(xl, xu, equ, expected_error,max_iterations):
+    
     x = sp.symbols('x')
     fx = parse_function(equ)
-    xr = 0.00
-    i = 0
-    error = 100
+
+    iterations = set_max_iterations_(max_iterations)
+    xr_old = 0.00
+
     fxl = fx.subs(x, xl)
     fxu = fx.subs(x, xu)
-    df = pd.DataFrame(columns=['Xl', 'Fxl', 'Xu', 'Fxu', 'Xr', 'Fxr', 'Error'])
-
-    while valid(fxu, fxl, equ):
-        xr_old = xr
+    df = pd.DataFrame(columns=['xl', 'f(xl)', 'xu', 'f(xu)', 'xr', 'f(xr)', 'Error'])
+    while valid(xu, xl, equ):
+        
         xr = xu - ((fxu * (xl - xu)) / (fxl - fxu))
         fxr = fx.subs(x, xr)
-        if i == 0 :
+        error = abs((xr - xr_old) / xr) * 100
+
+        if iterations == set_max_iterations_(max_iterations) :
             new_row_data = pd.DataFrame([{
-                'Xl': round(xl, 3), 'Fxl': round(fxl, 5), 'Xu': round(xu, 3), 'Fxu': round(fxu, 5),
-                'Xr': round(xr, 3), 'Fxr': round(fxr, 5), 'Error': "---"
+                'xl': xl, 'f(xl)': fxl, 'xu': xu, 'f(xu)': fxu,
+                'xr': xr, 'f(xr)': fxr, 'Error': "---"
             }])
         else:
             new_row_data = pd.DataFrame([{
-            'Xl': round(xl, 3), 'Fxl': round(fxl, 5), 'Xu': round(xu, 3), 'Fxu': round(fxu, 5),
-            'Xr': round(xr, 3), 'Fxr': round(fxr, 5), 'Error': round(error, 5)
+            'xl': xl, 'f(xl)': fxl, 'xu': xu, 'f(xu)': fxu,
+            'xr': xr, 'f(xr)': fxr, 'Error': error
             }])
-            error = abs((xr - xr_old) / xr) * 100
+
         df = pd.concat([df, new_row_data], ignore_index=True)
 
         if fxr * fxl < 0:
@@ -139,28 +166,33 @@ def false_position(xl, xu, equ, expected_error):
         elif fxr * fxl > 0:
             xl = xr
             fxl = fx.subs(x, xl)
-        else:
+
+        if error <= expected_error or iterations == 0:
             break
-        if error <= expected_error: break
-        i += 1
-    return df
+        iterations -= 1
+        xr_old = xr
+    
+    if df.empty: return None
+    else: return df
 
 
-def newton_method(initial, equ, expected_error):
+def newton_method(initial, equ, expected_error,max_iterations):
+    
     x = sp.symbols('x')
     fx = parse_function(equ)
     dfx = sp.diff(fx, x)
-    xi = round(initial, 3)
+    
+    xi = float(initial)
     error = 100
-    i = 0
+    iterations = set_max_iterations_(max_iterations)
     df = pd.DataFrame(columns=['X', 'f(x)', "f'(x)",'Error'])
     while True:
 
         fx_val = fx.subs(x, xi)
         dfx_val = dfx.subs(x, xi)
-        xi_plus1 = round(xi - (fx_val / dfx_val), 3)
+        xi_plus1 = xi - (fx_val / dfx_val)
 
-        if i == 0:
+        if iterations == set_max_iterations_(max_iterations):
             new_row_data = pd.DataFrame([{
                 'X' : xi, 'f(x)' : fx_val, "f'(x)":dfx_val, 'Error':"---"
             }])
@@ -170,34 +202,46 @@ def newton_method(initial, equ, expected_error):
             }])
         df = pd.concat([df, new_row_data], ignore_index=True)
 
-        if error <= expected_error: break
-
-        error = round(abs((xi_plus1 - xi) / xi_plus1) * 100, 3)
+        if error <= expected_error or iterations == 0:
+            break
+        error = abs((xi_plus1 - xi) / xi_plus1) * 100
         xi = xi_plus1
-        i += 1
-    return df
+        iterations -= 1
+    
+    if df.empty: return None
+    else: return df
 
-def se_cant(xi, xi_1, equ, expected_error):
+
+def se_cant(xi_1, x0, equ, expected_error,max_iterations):
+    
     x = sp.symbols('x')
     fx = parse_function(equ)
 
-    error = 100
-    i = 0
-    while error != expected_error:
-        xi_old = xi
+    iterations = set_max_iterations_(max_iterations)
+    df = pd.DataFrame(columns=['xi-1', 'f(xi-1)', 'xi', 'f(xi)', 'Error'])
 
-        fxi = float(fx.subs(x, xi))
-        fxi_1 = float(fx.subs(x, xi_1))
+    while True:
+        fxi_1 = fx.subs(x, xi_1)
+        fx0 = fx.subs(x, x0)
+        xiplus1 = x0 - fx0 * (( xi_1 - x0) / (fxi_1 - fx0))
+        error = abs((x0 - xi_1) / x0) * 100
+        if iterations == set_max_iterations_(max_iterations):
+            new_row_data = pd.DataFrame([{
+                'xi-1' : xi_1, 'f(xi-1)' : fxi_1, 'xi' : x0, 'f(xi)' : fx0, 'Error':"---"
+            }])
+        else:
+            new_row_data = pd.DataFrame([{
+                'xi-1': xi_1, 'f(xi-1)': fxi_1, 'xi': x0, 'f(xi)': fx0, 'Error': error
+            }])
+        df = pd.concat([df, new_row_data], ignore_index=True)
+        xi_1 = x0
+        x0 = xiplus1
 
-        xi_new = xi - (fxi * (xi_1 - xi)) / (fxi_1 - fxi)
-
-        error = abs((xi_new - xi_old) / xi_new) * 100
-
-        print(f"iteration:{i}|{xi_1:.3f}|{fxi_1:.3f}|{xi:.3f}|{fxi:.3f}|{error:.3f}")
-
-        xi_1 = xi
-        xi = xi_new
-        i += 1
+        if error <= expected_error or iterations == 0:
+            break
+        iterations -= 1
+    if df.empty: return None
+    else: return df
 
 
 def gauss_elimination(ab):
